578e2a9bc852746d01a93c27de04c6cedeb6bf7c
Matthew Kennedy <m.kennedy@me.com>
2021-01-06 13:16:56 +0000

    Make customizable

--- app/models/spree_product_reviews/configuration.rb
+++ app/models/spree_product_reviews/configuration.rb
@@ -1,5 +1,9 @@
 module SpreeProductReviews
   class Configuration < Spree::Preferences::Configuration
+    ########################################################
+    # These Setting are avalable in the admin UI
+    ########################################################
+
     # Include non-approved reviews in (public) listings.
     preference :include_unapproved_reviews, :boolean, default: false
 
@@ -19,9 +23,17 @@ module SpreeProductReviews
     # (name or email) on their review.
     preference :show_identifier, :boolean, default: false
 
+    ############################################################
+    # Set the preferences below in the spree.rb initializer file
+    ############################################################
+
     # How many star rating
     preference :number_of_stars, :integer, default: 5
 
+    # Limits the reviews to one review per user per product.
     preference :limit_to_one_review_per_user, :boolean, default: true
+
+    # Disable the default placment of review elements in the view files.
+    preference :disable_default_review_placment, :boolean, default: false
   end
 end
--- app/overrides/add_reviews_after_product_properties.rb
+++ app/overrides/add_reviews_after_product_properties.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
 Deface::Override.new(
   virtual_path: "spree/products/show",
   name: "converted_product_properties_767643482",
   insert_after: '[data-hook="product_properties"]',
   partial: "spree/shared/reviews",
-  disabled: false
+  disabled: SpreeProductReviews::Config[:disable_default_review_placment]
 )
 
 Deface::Override.new(
   virtual_path: "spree/products/_cart_form",
   name: "add_stars_under_price",
   insert_after: '[data-hook="product_price"]',
-  partial: "spree/shared/price_stars"
+  partial: "spree/shared/price_stars",
+  disabled: SpreeProductReviews::Config[:disable_default_review_placment]
 )
