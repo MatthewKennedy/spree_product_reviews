b035b2e8155a6a9fdf55427b09304b96824e2903
Matthew Kennedy <m.kennedy@me.com>
2021-01-25 16:31:48 +0000

    Update for 3.7 compatibility.

--- Appraisals
+++ Appraisals
@@ -1,3 +1,9 @@
+appraise "spree-3-7" do
+  gem "spree", branch: "3-7-stable", git: "https://github.com/spree/spree.git"
+  gem "spree_auth_devise", "~> 3.4.0"
+  gem "rails-controller-testing"
+end
+
 appraise "spree-4-2" do
   gem "spree", "~> 4.2.0.rc2"
   gem "spree_auth_devise", "~> 4.2.0"
--- app/overrides/add_reviews_after_product_properties.rb
+++ app/overrides/add_reviews_after_product_properties.rb
@@ -2,35 +2,26 @@
 
 if Spree.version.to_f < 4.0
   Deface::Override.new(
-    virtual_path: "spree/products/show",
-    name: "converted_product_properties_767643482",
-    insert_after: '[data-hook="product_properties"]',
-    partial: "spree/shared/reviews",
-    disabled: SpreeProductReviews::Config[:disable_default_review_placment]
-  )
-
-  Deface::Override.new(
     virtual_path: "spree/products/_cart_form",
-    name: "add_stars_under_price",
+    name: "add_stars_under_price_3_x",
     insert_before: '[data-hook="product_price"]',
     partial: "spree/shared/price_stars",
     disabled: SpreeProductReviews::Config[:disable_default_review_placment]
   )
 else
   Deface::Override.new(
-    virtual_path: "spree/products/show",
-    name: "converted_product_properties_767643482",
-    insert_after: '[data-hook="product_properties"]',
-    partial: "spree/shared/reviews",
-    disabled: SpreeProductReviews::Config[:disable_default_review_placment]
-  )
-
-  Deface::Override.new(
     virtual_path: "spree/products/_cart_form",
-    name: "add_stars_under_price",
+    name: "add_stars_under_price_4_x",
     insert_after: '[data-hook="product_price"]',
     partial: "spree/shared/price_stars",
     disabled: SpreeProductReviews::Config[:disable_default_review_placment]
   )
 end
 
+Deface::Override.new(
+  virtual_path: "spree/products/show",
+  name: "append_to_product_properties",
+  insert_after: '[data-hook="product_properties"]',
+  partial: "spree/shared/reviews",
+  disabled: SpreeProductReviews::Config[:disable_default_review_placment]
+)
